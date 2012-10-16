314804697d77af7e20ab2d75b6e19787438953cd
Thammanoon Sapprasom <thammanoongerman@gmail.com>
2012-10-16 19:29:45 +0200

    Fix the size of buttons

--- Sleep and Wake/src/com/example/sleep/and/wake/WakeupPanel.java
+++ Sleep and Wake/src/com/example/sleep/and/wake/WakeupPanel.java
@@ -34,11 +34,21 @@ public class WakeupPanel extends ControlPanel {
 		button_right_active = new Button(context);
 		button_left_shade = new Button(context);
 		button_right_shade = new Button(context);
-		
 		button_left_active.setBackgroundResource(R.raw.buttontestwake120x120);
 		button_right_active.setBackgroundResource(R.raw.buttontestwake600x120);
 		button_left_shade.setBackgroundResource(R.raw.buttontestwakeshade120x120);
 		button_right_shade.setBackgroundResource(R.raw.buttontestwakeshade600x120);
+		
+		// Set the buttons size 80x80 pixels 
+		// Maybe we can set the size of buttons corresponding to screen size of smart phone. etc.
+		// Somehow is possible to read the actual screen size -> but i don't know it now .
+		button_left_active.setLayoutParams(new LinearLayout.LayoutParams(80,80));
+		button_right_active.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,80));
+		button_left_shade.setLayoutParams(new LinearLayout.LayoutParams(80,80));
+		button_right_shade.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,80));
+		
+		
+		
 		
 		button_right_shade.setOnClickListener(new OnClickListener() {  
 			public void onClick(View view) { 
